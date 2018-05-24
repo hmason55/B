56068abf2fe3d1cdcb6f72a24e4a86f334c59842
Hunter Mason <hmason55@gmail.com>
2018-05-24 14:39:15 -0700

    Added draggable cards and basic card designer.

---
+++ B Project/Assets/Scripts/CardDesigner/Card.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class Card : MonoBehaviour {
+
+	public enum DeckClass {
+		Neutral,
+		Guardian,
+		Necromancer,
+		Thief,
+		Warrior
+	}
+
+	[SerializeField] string title;
+	[SerializeField] DeckClass deckType;
+	[SerializeField] int resourceCost;
+	[SerializeField] string description;
+
+	public bool showObjectReferences;
+
+	public Image backgroundImage;
+	public Image artworkImage;
+	public Text titleText;
+	public Image descriptionImage;
+	public Text descriptionText;
+	public Image costImage;
+	public Text costText;
+
+
+	void Update() {
+
+		if(backgroundImage) {
+			//backgroundImage.sprite = background;
+			switch(deckType) {
+				case DeckClass.Neutral:
+					backgroundImage.color = Color.gray;
+				break;
+
+				case DeckClass.Guardian:
+					// 130, 212, 219
+					backgroundImage.color = new Color(0.51f, 0.83f, 0.86f);
+				break;
+
+				case DeckClass.Necromancer:
+					// 186, 116, 201
+					backgroundImage.color = new Color(0.73f, 0.45f, 0.79f);
+				break;
+
+				case DeckClass.Thief:
+					// 187, 240, 87
+					backgroundImage.color = new Color(0.73f, 0.95f, 0.34f);
+				break;
+
+				case DeckClass.Warrior:
+					// 215, 67, 67
+					backgroundImage.color = new Color(0.84f, 0.26f, 0.27f);
+				break;
+			}
+		} 
+
+		if(artworkImage) {
+			//artworkImage.sprite = artwork;
+		}
+
+		if(titleText) {
+			titleText.text = title;
+		} 
+	
+		if(descriptionImage) {
+			//artworkImage.sprite = artwork;
+		}
+
+		if(descriptionText) {
+			descriptionText.text = description;
+		}
+
+		if(costImage) {
+			//costImage.sprite = artwork;
+		}
+
+		if(costText) {
+			if(resourceCost < 0) {
+				costText.text = "X";
+			} else {
+				costText.text = resourceCost.ToString();
+			}
+		}
+
+	}
+
+}
---
+++ B Project/Assets/Scripts/CardDesigner/CardEditor.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(Card))]
+[CanEditMultipleObjects]
+public class CardEditor : Editor {
+
+	SerializedProperty pTitle;
+	SerializedProperty pDeckType;
+	SerializedProperty pResourceCost;
+	SerializedProperty pDescription;
+
+	SerializedProperty pBackgroundImageObj;
+	SerializedProperty pArtworkImageObj;
+	SerializedProperty pTitleTextObj;
+	SerializedProperty pDescriptionImageObj;
+	SerializedProperty pDescriptionTextObj;
+	SerializedProperty pCostImageObj;
+	SerializedProperty pCostTextObj;
+
+	void OnEnable() {
+        // Setup the SerializedProperties.
+
+        pTitle = serializedObject.FindProperty("title");
+		pDeckType = serializedObject.FindProperty("deckType");
+        pResourceCost = serializedObject.FindProperty("resourceCost");
+		pDescription = serializedObject.FindProperty("description");
+        // Reference Objects
+        /*
+		pBackgroundImageObj = serializedObject.FindProperty("backgroundImage");
+		pArtworkImageObj = serializedObject.FindProperty("artworkImage");
+		pTitleTextObj = serializedObject.FindProperty("titleText");
+		pDescriptionImageObj = serializedObject.FindProperty("descriptionImage");
+		pDescriptionTextObj = serializedObject.FindProperty("descriptionText");
+		pCostImageObj = serializedObject.FindProperty("costImage");
+		pCostTextObj = serializedObject.FindProperty("costText");*/
+    }
+
+	public override void OnInspectorGUI() {
+		Card card = target as Card;
+
+        serializedObject.Update();
+
+		EditorGUILayout.PropertyField(pTitle, new GUIContent("Card Title"), null);
+		EditorGUILayout.PropertyField(pDeckType, new GUIContent("Deck Type"), null);
+		EditorGUILayout.IntSlider(pResourceCost, -1, 8, new GUIContent("Resource Cost"));
+		EditorGUILayout.PropertyField(pDescription, new GUIContent("Description"), null);
+
+		EditorGUILayout.Space();
+
+		if(GUILayout.Button("Save As Concept")) {
+			string prefabName = card.titleText.text;
+			Debug.Log(prefabName);
+			if(prefabName != null) {
+				string conceptPath = "Assets/Prefabs/Cards/Concepts/" + prefabName + ".prefab";
+				if(AssetDatabase.LoadAssetAtPath(conceptPath, typeof(GameObject))) {
+					if(EditorUtility.DisplayDialog("Hey! Listen!", "A card concept named \'" + prefabName + "\' already exists, do you want to overwrite it?", "Yes", "No")) {
+						CreateConcept(card.gameObject, conceptPath);
+					}
+				} else {
+					CreateConcept(card.gameObject, conceptPath);
+				}
+			}
+		}
+
+		EditorGUILayout.Space();
+		EditorGUILayout.Space();
+
+		card.showObjectReferences = EditorGUILayout.Toggle("Show Object References", card.showObjectReferences);
+
+		// Show or hide object references
+		if(card.showObjectReferences) {
+			card.backgroundImage = EditorGUILayout.ObjectField(new GUIContent("Background Image Asset"), card.backgroundImage, typeof(Image), true, null) as Image;
+			card.artworkImage = EditorGUILayout.ObjectField(new GUIContent("Artwork Image Asset"), card.artworkImage, typeof(Image), true, null) as Image;
+			card.titleText = EditorGUILayout.ObjectField(new GUIContent("Title Text Asset"), card.titleText, typeof(Text), true, null) as Text;
+			card.descriptionImage = EditorGUILayout.ObjectField(new GUIContent("Description Image Asset"), card.descriptionImage, typeof(Image), true, null) as Image;
+			card.descriptionText = EditorGUILayout.ObjectField(new GUIContent("Description Text Asset"), card.descriptionText, typeof(Text), true, null) as Text;
+			card.costImage = EditorGUILayout.ObjectField(new GUIContent("Cost Image Asset"), card.costImage, typeof(Image), true, null) as Image;
+			card.costText = EditorGUILayout.ObjectField(new GUIContent("Cost Text Asset"), card.costText, typeof(Text), true, null) as Text;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    // Custom GUILayout progress bar.
+    void ProgressBar (float value, string label) {
+        // Get a rect for the progress bar using the same margins as a textfield:
+        //Rect rect = GUILayoutUtility.GetRect (18, 18, "TextField");
+        //EditorGUI.ProgressBar (rect, value, label);
+        //EditorGUILayout.Space ();
+    }
+
+    static void CreateConcept(GameObject obj, string path) {
+    	Object prefab = PrefabUtility.CreatePrefab(path, obj);
+    	PrefabUtility.ReplacePrefab(obj, prefab, ReplacePrefabOptions.ConnectToPrefab);
+    }
+}
