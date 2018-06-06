70585c0ba53a0b0d2eb96153542c5fd5e1a8c378
Hunter Mason <hmason55@gmail.com>
2018-06-06 13:37:18 -0700

    Added new Card Designer features.
    Organized existing card attributes into sections.
    
    Increased the height of the "Description" text area.
    
    Added "Target Type" field for the "Require Target" attribute.
    
    Added "Area of Effect" attribute.
    
    Added a 3x3 grid field to create an area target for the "Area of Effect" attribute.
    
    Added support for card Effects.
    
    Effects currently have the following fields: "Effect", "Target", "Value", "Duration".
    
    The current options for these fields are:
    
    Effect: Damage, Block, Heal, Stun, Push, Resource.
    Target: None, Self, Enemy, Ally, RandomEnemy, Random Ally.
    Value: Integer -1 to 20.
    Duration: Integer -1 to 20.
    
    All card data has been updated to support the new attributes, but not all the cards currently use the new attributes. (I've only changed Cleave, Sword and Board, Slice).

--- B Project/Assets/Scripts/CardDesigner/CardData.cs
+++ B Project/Assets/Scripts/CardDesigner/CardData.cs
@@ -12,4 +12,14 @@ public class CardData {
 	public string Description;
 	public bool OmitFromDeck;
 	public bool RequireTarget;
+	public bool AreaOfEffect;
+	public bool[] TargetArea = new bool[9];
+	public Card.TargetType TargetType;
+	//public List<Card.Effect> Effects;
+	public List<Card.Effect> Effects;
+}
+
+[Serializable]
+public class EffectsWrapper {
+	public List<Card.Effect> list;
 }
