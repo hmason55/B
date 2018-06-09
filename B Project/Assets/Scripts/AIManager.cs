c2ed1968d79f3885f706fc1045de7c2393fde604
Davide Wu <davwww@fastwebnet.it>
2018-06-09 22:58:37 +0200

    Basic rough flow.

---
+++ B Project/Assets/Scripts/AIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIManager : MonoBehaviour
+{
+    
+
+    public void StartAITurn()
+    {
+        // cycle AI units
+
+        // Pick a target for the card
+
+        // play the card
+
+        // end turn
+
+    }
+
+    public void DealAICards()
+    {
+        // Deal cards
+
+        // Show UI on enemies
+
+    }
+
+}
