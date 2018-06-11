5c3f0d2acf16a9ce7c44a569a06f5dcb9cb66f45
Davide Wu <davwww@fastwebnet.it>
2018-06-11 22:52:30 +0200

    More on AI and turn manager.

--- B Project/Assets/Scripts/TurnManager.cs
+++ B Project/Assets/Scripts/TurnManager.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class TurnManager : MonoBehaviour
 {
-
     private bool _playerTurn = true;
+
+    // AI Manager cache
+    private AIManager _aiManager;
+    // Battleground cache
+    private Battleground _battleground;
+
+    void Start()
+    {
+        _aiManager = FindObjectOfType<AIManager>();
+        _battleground = FindObjectOfType<Battleground>();
+    }
 
     public void StartGame()
     {
@@ -24,12 +34,11 @@ public class TurnManager : MonoBehaviour
         // Turn hand UI and cards on
 
         // Enable battleground targeting
+        _battleground.SetTargetShape(TargetShape.Single);
 
         // Draw next AI cards and show them 
-
-        // Disable AI
-
-        // ...
+        _aiManager.DealAICards();
+        
 
     }
 
@@ -38,10 +47,21 @@ public class TurnManager : MonoBehaviour
         // Turn hand UI off
 
         // Disable battleground targeting
+        _battleground.SetTargetShape(TargetShape.None);
 
         // Enable AI
+        StartCoroutine(_aiManager.StartAITurn());
     }
 
-    
+
+    public void BattleWon()
+    {
+        Debug.Log("Battle is over you won!!!");
+    }
+
+    public void BattleLost()
+    {
+        Debug.Log("Battle is lost, game over!");
+    }
 
 }
